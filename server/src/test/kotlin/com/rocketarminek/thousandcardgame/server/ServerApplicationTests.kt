42b60ac9450e5a0db575c93ecdb374a1c6d174c2
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2020-09-16 23:13:42 +0200

    [Server] init app

---
+++ server/src/test/kotlin/com/rocketarminek/thousandcardgame/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rocketarminek.thousandcardgame.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
