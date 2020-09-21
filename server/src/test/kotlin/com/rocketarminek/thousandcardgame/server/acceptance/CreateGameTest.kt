ad0061b66b6eb536878a24c198eda9287f4bc7be
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2020-09-22 00:44:36 +0200

    [Server][Game] Create game with reactive store

--- server/src/test/kotlin/com/rocketarminek/thousandcardgame/server/acceptance/CreateGameTest.kt
+++ server/src/test/kotlin/com/rocketarminek/thousandcardgame/server/acceptance/CreateGameTest.kt
@@ -4,12 +4,10 @@ import com.rocketarminek.thousandcardgame.server.game.infrastructure.Configurati
 import com.rocketarminek.thousandcardgame.server.game.infrastructure.controller.GetEventsAction
 import com.rocketarminek.thousandcardgame.server.game.infrastructure.controller.Response
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldContain
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.support.AnnotationConfigContextLoader
@@ -21,18 +19,6 @@ internal class CreateGameTest(@Autowired val client: TestRestTemplate) {
     fun `it creates game`() {
         val createResponse = client.postForEntity("/v1/games", null, Response::class.java)
         createResponse.statusCode shouldBeEqualTo HttpStatus.CREATED
-        createResponse.body?.id shouldBeEqualTo "671e7abd-5b20-425a-a70e-df1aa9142e0c"
-
-        val result = client.getForEntity<String>("/v1/games/${createResponse.body?.id}/events")
-        result.statusCode shouldBeEqualTo HttpStatus.OK
-        result.body?.toString()!! shouldContain "671e7abd-5b20-425a-a70e-df1aa9142e0c"
-        result.body?.toString()!! shouldContain "game-created"
-    }
-
-    @Test
-    fun `it cannot be created with same id`() {
-        client.postForEntity("/v1/games", null, Response::class.java)
-        val createResponse = client.postForEntity("/v1/games", null, Response::class.java)
-        createResponse.statusCode shouldBeEqualTo HttpStatus.BAD_REQUEST
+        createResponse.body?.message shouldBeEqualTo "Created"
     }
 }
--- server/src/test/kotlin/com/rocketarminek/thousandcardgame/server/unit/GameRepositorySpec.kt
+++ server/src/test/kotlin/com/rocketarminek/thousandcardgame/server/unit/GameRepositorySpec.kt
@@ -2,28 +2,37 @@ package com.rocketarminek.thousandcardgame.server.unit
 
 import com.rocketarminek.thousandcardgame.server.game.domain.model.Game
 import com.rocketarminek.thousandcardgame.server.game.infrastructure.repository.EventSourcedGameRepository
+import com.rocketarminek.thousandcardgame.server.shared.EventStore
 import com.rocketarminek.thousandcardgame.server.shared.InMemoryEventStore
+import com.rocketarminek.thousandcardgame.server.shared.ReactiveEventStore
 import com.rocketarminek.thousandcardgame.server.shared.Repository
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import reactor.core.publisher.Flux
 
 object GameRepositorySpec: Spek({
     describe("The game repository") {
-        val repository: Repository<Game> by memoized { EventSourcedGameRepository(InMemoryEventStore()) }
+        val reactiveEventStore: ReactiveEventStore = mockk()
+        val inMemoryEventStore: EventStore by memoized { InMemoryEventStore() }
+        val repository: Repository<Game> by memoized { EventSourcedGameRepository(reactiveEventStore, inMemoryEventStore) }
+
         it("can save the game") {
-            repository.save(Game("#123", arrayOf("#22", "#32", "#34")))
+            val game = Game("#123", arrayOf("#22", "#32", "#34"))
+            justRun { reactiveEventStore.save(game.uncommittedChanges) }
+            every { reactiveEventStore.load(game.id) } returns Flux.fromIterable(game.uncommittedChanges)
+
+            repository.save(game)
             repository.find("#123")?.id shouldBeEqualTo "#123"
             repository.find("#123")?.playerIds shouldBeEqualTo arrayOf("#22", "#32", "#34")
+            repository.find("#123")?.uncommittedChanges shouldBeEqualTo arrayListOf()
         }
         it("returns null if cannot find any game") {
             repository.find("#123") shouldBe null
-        }
-        it("commits all changes") {
-            val game = Game("#123", arrayOf("#22", "#32", "#34"));
-            repository.save(game)
-            repository.find("#123")?.uncommittedChanges shouldBeEqualTo arrayListOf()
         }
     }
 })
