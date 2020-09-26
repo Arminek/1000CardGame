19af6da36c70aec6e648bd44e392bb6b478cbe2e
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2020-09-26 16:12:56 +0200

    [Server][Game] Declaring the bid

--- server/src/main/kotlin/com/rocketarminek/thousandcardgame/server/game/domain/event/Events.kt
+++ server/src/main/kotlin/com/rocketarminek/thousandcardgame/server/game/domain/event/Events.kt
@@ -5,10 +5,30 @@ import com.rocketarminek.thousandcardgame.server.game.domain.model.GameId
 import com.rocketarminek.thousandcardgame.server.game.domain.model.PlayerId
 import com.rocketarminek.thousandcardgame.server.shared.Event
 
-data class GameCreated(override val id: GameId, val playerIds: ArrayList<PlayerId>, val type: String = "game-created") : Event
-data class BidStarted(override val id: GameId, val bidId: BidId, val playerIds: ArrayList<PlayerId>, val type: String = "bid-started") : Event
-data class BidWon(override val id: GameId, val bidId: BidId, val playerId: PlayerId, val amount: Int, val type: String = "bid-won") : Event
-data class BidPassed(override val id: GameId, val bidId: BidId, val playerId: PlayerId, val type: String = "bid-passed") : Event
+data class GameCreated(
+        override val id: GameId,
+        val playerIds: ArrayList<PlayerId>,
+        val type: String = "game-created"
+) : Event
+data class BidStarted(
+        override val id: GameId,
+        val bidId: BidId,
+        val playerIds: ArrayList<PlayerId>,
+        val type: String = "bid-started"
+) : Event
+data class BidWon(
+        override val id: GameId,
+        val bidId: BidId,
+        val playerId: PlayerId,
+        val amount: Int,
+        val type: String = "bid-won"
+) : Event
+data class BidPassed(
+        override val id: GameId,
+        val bidId: BidId,
+        val playerId: PlayerId,
+        val type: String = "bid-passed"
+) : Event
 data class BidIncreased(
         override val id: GameId,
         val bidId: BidId,
@@ -16,4 +36,15 @@ data class BidIncreased(
         val amount: Int,
         val type: String = "bid-increased"
 ) : Event
-data class TurnStarted(override val id: GameId, val playerId: PlayerId, val type: String = "turn-started"): Event
+data class BidDeclared(
+        override val id: GameId,
+        val bidId: BidId,
+        val playerId: PlayerId,
+        val amount: Int,
+        val type: String = "bid-declared"
+): Event
+data class TurnStarted(
+        override val id: GameId,
+        val playerId: PlayerId,
+        val type: String = "turn-started"
+): Event
