c05a2ae6e6faaf0de9ca9161cff3c3710367deed
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2020-11-09 22:14:57 +0100

    [Infrastructure] Manually switch image version

--- server/src/main/kotlin/com/rocketarminek/thousandcardgame/server/game/infrastructure/controller/HealthAction.kt
+++ server/src/main/kotlin/com/rocketarminek/thousandcardgame/server/game/infrastructure/controller/HealthAction.kt
@@ -1,8 +1,5 @@
 package com.rocketarminek.thousandcardgame.server.game.infrastructure.controller
 
-import com.rocketarminek.thousandcardgame.server.game.domain.model.Game
-import com.rocketarminek.thousandcardgame.server.shared.Repository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -10,16 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HealthAction(@Autowired private val repository: Repository<Game>) {
+class HealthAction() {
     @CrossOrigin
     @GetMapping(value = ["/_health"])
-    fun health(): ResponseEntity<Response> {
+    fun health(): ResponseEntity<Any> {
         return ResponseEntity(HttpStatus.OK)
     }
 
     @CrossOrigin
     @GetMapping(value = ["/_ready"])
-    fun ready(): ResponseEntity<Response> {
+    fun ready(): ResponseEntity<Any> {
         return ResponseEntity(HttpStatus.OK)
     }
 }
